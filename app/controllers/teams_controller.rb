2e82aea3000dfddf0bacb3ee56a619a176ed8a55
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-21 17:37:02 -0400

    adds home page and resources for routes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,9 @@
 class TeamsController < ApplicationController
+  def index
+    @teams = Team.all
+    respond_to do |format|
+      format.html
+      format.json{ render json: @teams, status: :ok}
+    end
+  end
 end

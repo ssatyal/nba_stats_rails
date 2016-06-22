ab5b698bd4ce9c38249e3256a711d0b3d86c6817
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-22 13:55:54 -0400

    starts seed data for teams, 3 down

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Team.destroy_all
+
+gsw = Team.create(team: "Golden State Warriors", pointspg: 114.9,
+dpointspg: 104.1,
+fgpct: .487,
+threeptpct: .416,
+assistspg: 28.9)
+
+okc = Team.create(team: "Oklahoma City Thunder",
+pointspg: 110.2,
+dpointspg: 102.9,
+fgpct: .476,
+threeptpct: .348,
+assistspg: 23.0)
+
+sac = Team.create(team: "Sacramento Kings",
+pointspg: 106.6,
+dpointspg: 109.1,
+fgpct: .464,
+threeptpct: .359,
+assistspg: 24.5)
+
+# Team.create(team: ,
+# pointspg: ,
+# dpointspg: ,
+# fgpct: ,
+# threeptpct: ,
+# assistspg: )
